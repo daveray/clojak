474213438bdb5f50ae200d5f6089c8ab9c0e94cb
Dave Ray <daveray@gmail.com>
2011-11-17 18:44:10 -0500

    Use Clojure 1.3

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject clojak "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [se.scalablesolutions.akka/akka-actor "1.1"]]
   :repositories {"Akka" "http://akka.io/repository"}
   :aot [clojak.pi])
